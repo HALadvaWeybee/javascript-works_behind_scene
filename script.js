8bc7b047d1eb89bfbb04bb7ca7b15b973fc8cf4f
himanshu@webtheuniverse.com <himanshu@webtheuniverse.com>
2022-06-28 19:05:33 +0530

    complete scopping on practice with handson

---
+++ script.js
@@ -0,0 +1,35 @@
+'use strict';
+
+// SCOPING IN PRATICE
+/* function calcAge(birthYear) {
+    const age = 2037 - birthYear;
+
+    function printAge() {
+        const output= `${firstName}, You are ${age}, born in ${birthYear}`;
+        console.log(output);
+
+        if(birthYear >= 1981 && birthYear <= 1996) {
+            var himanshu = true;
+            const firstName = 'ladva';
+
+            output = 'NEW OUTPUT!';
+            const str = `Oh, and you are a himanshu , ${firstName}`;
+            console.log(str);
+
+            function add(a,b) {
+                return a+b;
+            }
+        }
+        // console.log(str);
+        console.log(himanshu);
+        // console.log(add(2,3)); 
+    }
+    printAge();
+
+    return age;
+}
+
+const firstName = 'himanshu';
+calcAge(1991); 
+// console.log(age);
+// printAge() */
